23ea23c49137410cf912ab063115763872b669bc
Alok <105195422+Alok-112@users.noreply.github.com>
2024-02-21 13:01:45 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/',
-  build: {
-    outDir: 'public', // Specify the output directory
-  },
+
   plugins: [react()]
 });
